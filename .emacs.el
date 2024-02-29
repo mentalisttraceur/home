mentalisttraceur/home

commit f697c68a59e52dbfa0199f1619af07ea1bea425c

.emacs: make undo-tree window not "dedicated"
The only difference I see from making it not
be a ("soft") dedicated window is that it
shares space with other window splits as I'd
expect - doesn't refuse/resist rebalancing.

In particular, this change makes it so that
any pop-to-buffer call just behaves like a
normal pop-to-buffer - creating a window split
only if none exist and reusing the LRU one
otherwise. (Instead of the default behavior,
which caused any `pop-to-buffer` to split the
remaining half of the screen if undo tree was
already occupying one half.)

.emacs.el
@@ -1395,6 +1395,7 @@
     (add-to-list 'undo-tree-incompatible-major-modes
         'undo-tree-visualizer-mode)
     (add-to-list 'undo-tree-visualizer-mode-hook (lambda ()
+        (set-window-dedicated-p (selected-window) nil)
         (setq mode-name "Undo")))
     (define-key undo-tree-visualizer-mode-map
         "\C-m" 'undo-tree-visualizer-quit)

commit f9e86025d9d0732ba4a637eb18039658164dfc72

.emacs.el: make undo-tree window behave "better"
Fixes the edge case (common for me on mobile)
where quitting undo-tree would fail to restore
the single window layout, introduced by commit
f697c68a59e52dbfa0199f1619af07ea1bea425c where
I made the undo tree window not "dedicated".

.emacs.el
@@ -1366,9 +1366,12 @@
     (setq undo-tree-auto-save-history nil)
     (add-to-list 'undo-tree-incompatible-major-modes
         'undo-tree-visualizer-mode)
-    (add-to-list 'undo-tree-visualizer-mode-hook (lambda ()
-        (set-window-dedicated-p (selected-window) nil)
-        (setq mode-name "Undo")))
+    (defun hack-undo-tree-visualize (undo-tree-visualize &rest arguments)
+        (with-advice ('switch-to-buffer-other-window
+                          :override 'pop-to-buffer)
+            (apply undo-tree-visualize arguments))
+        (setq mode-name "Undo"))
+    (advice-add 'undo-tree-visualize :around 'hack-undo-tree-visualize)
     (define-key undo-tree-visualizer-mode-map
         "\C-m" 'undo-tree-visualizer-quit)
     (define-key undo-tree-visualizer-mode-map