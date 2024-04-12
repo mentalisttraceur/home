980fa53bfa3bc2241d5e554600351824119b39ae
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2024-04-12 03:06:09 +0000

    .emacs.el: move config lines to the right place

--- .emacs.el
+++ .emacs.el
@@ -1767,6 +1767,10 @@
 (use-package corfu
     :config
     (global-corfu-mode 1)
+    (setq corfu-scroll-margin 0)
+    (setq corfu-preview-current nil)
+    (defun corfu--preview-current-p ()
+        t)
     (defun fixed-corfu-quit (&rest _)
         (run-hooks 'buffer-list-update-hook))
     (advice-add 'corfu-quit :after 'fixed-corfu-quit)
@@ -2746,10 +2750,6 @@
 
 (use-packages corfu evil
     :config
-    (setq corfu-scroll-margin 0)
-    (setq corfu-preview-current nil)
-    (defun corfu--preview-current-p ()
-        t)
     (defun evil-corfu-escape ()
         (lambda-let ((original-escape-action
                          (or (lookup-key evil-normal-state-local-map [escape])
