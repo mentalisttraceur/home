mentalisttraceur/home

commit 7f0615ba08ad96881fe3c68b45372cb90021a05b

.emacs.el: fixed-decoded-time-add v3
Handle zero/nil offsets by totally skipping
the addition code for that offset instead
of letting it just mathematically work out
as a no-op.

More so than optimizing, this enables valid
operations on partially undefined datetimes!
(That's huge for making datetime arithmetic
more generally useful and reusable in more
situations with less boilerplate. For example,
my datetime parse and read functions can use
a decoded time object with all fields set to
nil, only set the parts that are specified,
and still use fixed-decoded-time-add, instead
of needing to populate the unspecified fields
with valid values but then remember elsewhere
that they weren't set yet.)

.emacs.el
@@ -1477,21 +1477,23 @@
         (aref table day)))
 
 (defmacro datetime--year+ ()
-    `(setq year (+ year year+)))
+    `(unless (= year+ 0)
+         (setq year (+ year year+))))
 
 (defmacro datetime--month+ ()
-    `(let ((days-in-month (datetime-days-in-month year month)))
-         (setq month (+ month month+))
-         (if (> month 0)
-             (setq year+ (/ (- month 1) 12))
-             (setq month (- month 12))
-             (setq year+ (/ month 12)))
-         (setq month (1+ (mod (1- month) 12)))
-         (datetime--year+)
-         (setq day (min day (datetime-days-in-month year month)))))
+    `(unless (= month+ 0)
+         (let ((days-in-month (datetime-days-in-month year month)))
+             (setq month (+ month month+))
+             (if (> month 0)
+                 (setq year+ (/ (- month 1) 12))
+                 (setq month (- month 12))
+                 (setq year+ (/ month 12)))
+             (setq month (1+ (mod (1- month) 12)))
+             (datetime--year+)
+             (setq day (min day (datetime-days-in-month year month))))))
 
 (defmacro datetime--day+ ()
-    `(progn
+    `(unless (= day+ 0)
          (setq day (datetime-ordinal-day year month day))
          (setq day (+ day day+))
          (if (> day 0)
@@ -1507,7 +1509,7 @@
          (setq day (- day (datetime-days-until-month year month)))))
 
 (defmacro datetime--hour+ ()
-    `(progn
+    `(unless (= hour+ 0)
          (datetime--validate-hour hour)
          (setq hour (+ hour hour+))
          (if (>= hour 0)
@@ -1518,7 +1520,7 @@
          (datetime--day+)))
 
 (defmacro datetime--minute+ ()
-    `(progn
+    `(unless (= minute+ 0)
          (datetime--validate-minute minute)
          (setq minute (+ minute minute+))
          (if (>= minute 0)
@@ -1529,7 +1531,7 @@
          (datetime--hour+)))
 
 (defmacro datetime--second+ ()
-    `(progn
+    `(unless (= second+ 0)
          (datetime--validate-second second)
          (setq second (+ second second+))
          (if (>= second 0)

commit a775d7d8a66f94561a4e404d4a0317f000391087

.emacs.el: fixed-decoded-time-add: fix leap day correction
This fixes a bug introduced in commit
7f0615ba08ad96881fe3c68b45372cb90021a05b.

Adding 1 year to a February 29th should land on
February 28th (or arguably Marh 1st) of the next
year, but the change in that commit prevents
that normalization from happening unless there's
also a day offset.

This adds an explicit separate correction, not
part of the normal arithmetic, which runs only
when a year and/or month delta results in a
February 29th, before performing any of the
lower-magnitude/higher-precision arithmetic.

.emacs.el
@@ -1579,6 +1579,11 @@
         (setq-if-nil second+ 0)
         (datetime--year+)
         (datetime--month+)
+        (when (and (or year+ month+)
+                   (= month 2)
+                   (= day 29)
+                   (not (datetime-is-leap year)))
+            (setq day 28))
         (datetime--day+)
         (datetime--hour+)
         (datetime--minute+)