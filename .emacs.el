72454d3d4ca7e50d3928795b7663fefad3162d2c
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2024-08-27 16:08:29 +0000

    .emacs.el: work around bug in native-compile-async-skip-p
    `native-compile-async-skip-p` fails to match files which
    end with a second wrapper extension, such as`.el.gz`
    instead of just `.el`.

--- .emacs.el
+++ .emacs.el
@@ -41,6 +41,18 @@
       file-name-handler-alist nil)
 
 
+(defun fixed-native-compile-async-skip-p
+        (native-compile-async-skip-p file load selector)
+    (let* ((naive-elc-file (file-name-with-extension file "elc"))
+           (elc-file       (replace-regexp-in-string
+                               "\\.el\\.elc$" ".elc" naive-elc-file)))
+        (or (gethash elc-file comp--no-native-compile)
+            (funcall native-compile-async-skip-p file load selector))))
+
+(advice-add 'native-compile-async-skip-p
+    :around 'fixed-native-compile-async-skip-p)
+
+
 (when wsl
     (defun replace-invalid-unicode-1 (character)
         (if (> character #x10FFFF)
