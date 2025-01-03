cd6e75d98e92aae34fb9e3bb9b7f2238bef9853c
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2025-01-03 01:12:12 +0000

    .emacs.el: fixed-field-at-position: secondary return: beginning and end
    1. Lots of code that uses fields needs field
       boundaries (including the actually correct
       versions of my other field functions).
    
    2. A separate boundary-getting function would
       be mostly code duplication.
    
    3. I gave myself a proper `values` and
       `multiple-values-bind` - I might as
       well enjoy them.

--- .emacs.el
+++ .emacs.el
@@ -1174,12 +1174,13 @@
         (let ((field-1 (get-char-property beginning 'field))
               (field-2 (get-char-property (1- end)  'field)))
             (if (eq field-1 field-2)
-                field-1
+                (values field-1 beginning end)
+                (setq-if-nil position (point))
                 (if (rear-nonsticky-p 'field position)
                     (if (front-sticky-p 'field position)
-                        field-2
-                        nil)
-                    field-1)))))
+                        (values field-2 position end)
+                        (values nil position position))
+                    (values field-1 beginning position))))))
 
 (defun fixed-field-string (field-string &optional position)
     (if (fixed-field-at-position position)
