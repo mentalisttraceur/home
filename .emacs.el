2f7affe1585c739626cd2778bcfcdf77f0c7926f
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2025-01-29 07:20:06 +0000

    .emacs.el: music: fix "o"/"p" move edge-cases
    This fixes edge-cases at the bottom of the
    buffer and when pasting enough lines to
    span/displace the currently playing song's
    lines. For example, `p` or `o` at the very
    end of the buffer (no song on current line)
    will now place point correctly on the last
    song (and this is the base case, when first
    adding files to a new/empty music buffer).

--- .emacs.el
+++ .emacs.el
@@ -7563,7 +7563,7 @@
 (evil-define-command music-paste-after (count register)
     :suppress-operator t
     (interactive "p<x>")
-    (music--line-move-after-refresh
+    (music--index-move-after-refresh
         (music--paste count register 1)))
 (music-define-key 'normal "p" 'music-paste-after)
 (evil-define-command music-paste-before (count register)
@@ -7585,13 +7585,13 @@
 (evil-define-command music-replacing-paste-after (count register)
     :suppress-operator t
     (interactive "p<x>")
-    (music--line-move-after-refresh
+    (music--index-move-after-refresh
         (music-replacing-paste-before count register)))
 (music-define-key 'normal "gp" 'music-replacing-paste-after)
 (music-define-key 'normal "gP" 'music-replacing-paste-before)
 (defun music-open-after (count)
     (interactive "p")
-    (music--line-move-after-refresh
+    (music--index-move-after-refresh
         (music--open count (music-select) (music--index-for-point 1))))
 (music-define-key 'normal "o" 'music-open-after)
 (defun music-open-before (count)
