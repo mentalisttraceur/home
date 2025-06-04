937c71e213b2cde15b725dc7286e61848038668f
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2025-06-04 21:54:14 +0000

    .emacs.el: internal naming cleanup

--- .emacs.el
+++ .emacs.el
@@ -54,16 +54,16 @@
       file-name-handler-alist nil)
 
 
-(defun fixed-native-compile-async-skip-p
-        (native-compile-async-skip-p file load selector)
+(defun fixed-native--compile-async-skip-p
+        (native--compile-async-skip-p file load selector)
     (let* ((naive-elc-file (file-name-with-extension file "elc"))
            (elc-file       (replace-regexp-in-string
                                "\\.el\\.elc$" ".elc" naive-elc-file)))
         (or (gethash elc-file comp--no-native-compile)
-            (funcall native-compile-async-skip-p file load selector))))
+            (funcall native--compile-async-skip-p file load selector))))
 
 (advice-add 'native--compile-async-skip-p
-    :around 'fixed-native-compile-async-skip-p)
+    :around 'fixed-native--compile-async-skip-p)
 
 
 (set-char-table-range char-width-table ?👁 2)
