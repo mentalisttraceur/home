7da0dc9a97c48360894f1aa511cb701039c25306
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2026-09-03 17:50:34 +0000

    Emacs: music: show URLs correctly
    Don't run URLs through the file path logic.
    
    (I don't much care for playing URLs, but it's
    nice as a quick test/demo since `mpv` can do
    it natively, nice to have on-hand just-in-case
    as a capability, plus I have a user who has an
    interest in streaming internet radio.)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8734,9 +8734,13 @@
 (defun music--file-line (socket index)
     (let* ((format (format "${playlist/%d/filename}" index))
            (path (mpv-ipc-expand socket format))
-           (file (file-name-nondirectory path))
-           (path (expand-file-name path))
+           (url-type (url-type (url-generic-parse-url path)))
+           (file (if url-type
+                     path
+                     (file-name-nondirectory path)))
            (file-line (concat file "\n")))
+        (unless url-type
+            (setq path (expand-file-name path)))
         (propertize file-line 'mpv-index index 'full-path path)))
 (defvar music--playing-line-map (make-sparse-keymap))
 (defun music--playing-line-fix-point (event)
